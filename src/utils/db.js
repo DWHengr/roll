b5631a786dfa15f96d896aee526a81e2926e03aa
Heath <duweihengmr@163.com>
2024-04-16 20:05:04 +0800

    sets

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,5 +7,27 @@ db.version(1).stores({
     //记录表
     records: '++id, round, record, type',
     //剩余内容表
-    residual: '++id, content, type'
+    residual: '++id, content, type',
+    //系统设置
+    sets: 'name, content',
 });
+
+const amount = await db.sets.where('name').equals('amount').toArray();
+if (amount.length === 0) {
+    await db.sets.add({name: 'amount', content: 1});
+}
+
+const interval = await db.sets.where('name').equals('interval').toArray();
+if (interval.length === 0) {
+    await db.sets.add({name: 'interval', content: 5});
+}
+
+const fontSize = await db.sets.where('name').equals('fontSize').toArray();
+if (fontSize.length === 0) {
+    await db.sets.add({name: 'fontSize', content: 80});
+}
+
+const isRepeat = await db.sets.where('name').equals('isRepeat').toArray();
+if (isRepeat.length === 0) {
+    await db.sets.add({name: 'isRepeat', content: true});
+}
